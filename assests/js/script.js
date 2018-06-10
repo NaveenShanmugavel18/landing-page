43dacd77fc7ce3a7831b47a776428878928c4e57
Naveen Shanmugavel <naveensv18@gmail.com>
2018-06-10 16:27:18 +0530

    html completed

---
+++ assests/js/script.js
@@ -0,0 +1,84 @@
+$(document).ready(function(){
+	
+	/* Sticky navigation */
+	$('.js--section-features').waypoint(function(direction) {
+		if(direction == "down") {
+			$('nav').addClass('sticky');
+		} else {
+			$('nav').removeClass('sticky');
+		}
+	}, {
+		offset: '60px;'
+	});
+
+	/* Scroll to button */
+	$('.js--scroll-to-plans').on('click', function() {
+		$('html, body').animate({
+			scrollTop: $('.js--section-plans').offset().top
+		}, 1000);
+	});
+
+	$('.js--scroll-to-start').on('click', function() {
+		$('html, body').animate({
+			scrollTop: $('.js--section-features').offset().top
+		}, 1000);
+	});
+
+	/* Navigation Scroll */
+	$(function() {
+		$('a[href*="#"]:not([href="#"])').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				if (target.length) {
+					$('html, body').animate({
+						scrollTop: target.offset().top
+					}, 1000);
+				
+					return false;
+				}
+			}
+		});
+	});
+
+	/* Animations on Scroll */
+	$('.js--wp-1').waypoint(function(direction) {
+		$('.js--wp-1').addClass('animated fadeIn');
+	}, {
+		offset: '50%'
+	});
+
+	$('.js--wp-2').waypoint(function(direction) {
+		$('.js--wp-2').addClass('animated fadeInUp');
+	}, {
+		offset: '50%'
+	});
+
+	$('.js--wp-3').waypoint(function(direction) {
+		$('.js--wp-3').addClass('animated fadeIn');
+	}, {
+		offset: '50%'
+	});
+
+	$('.js--wp-4').waypoint(function(direction) {
+		$('.js--wp-4').addClass('animated pulse');
+	}, {
+		offset: '50%'
+	});
+
+	/* Mobile nav */
+	$('.js--nav-icon').on('click', function() {
+		var nav = $('.js--main-nav');
+		var icon = $('.js--nav-icon i');
+		nav.slideToggle(200);
+
+		if (icon.hasClass('ion-navicon-round')) {
+			icon.addClass('ion-close-round');
+			icon.removeClass('ion-navicon-round');
+		} else {
+			icon.addClass('ion-navicon-round');
+			icon.removeClass('ion-close-round');
+		}
+	});
+
+});
